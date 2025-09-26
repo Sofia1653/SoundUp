d055c40d867aca7eb582df4e2fa9f14cb91c7c06
biacosta7 <contact.beatrizcosta@gmail.com>
2025-09-26 09:19:10 -0300

    initializing project

---
+++ src/main/java/com/soundup/soundup/SoundupApplication.java
@@ -0,0 +1,13 @@
+package com.soundup.soundup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soundup/soundup/SoundupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soundup.soundup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
