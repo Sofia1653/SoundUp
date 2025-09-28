d08d25e17420a90edb586111af6d3a769834f6dc
biacosta7 <contact.beatrizcosta@gmail.com>
2025-09-28 06:41:13 -0300

    MusicaController and MusicaService Changes by ninafernandes

---
+++ backend/src/main/java/com/soundup/soundup/controller/MusicaController.java
@@ -0,0 +1,45 @@
+package com.soundup.soundup.controller;
+
+import com.soundup.soundup.model.Musica;
+import com.soundup.soundup.model.Usuario;
+import com.soundup.soundup.service.MusicaService;
+import com.soundup.soundup.service.UsuarioService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/musicas")
+public class MusicaController {
+    private final MusicaService MusicaService;
+    public MusicaController(MusicaService MusicaService) {
+        this.MusicaService = MusicaService;
+    }
+
+    @GetMapping
+    public List<Musica> getMusicas() {
+        return MusicaService.getAllMusicas();
+    }
+
+    @GetMapping("/{id}")
+    public Musica getMusica(@PathVariable int id) {
+        return MusicaService.getMusicaById(id);
+    }
+
+    @PostMapping
+    public void addMusica(@RequestBody Musica musica) {
+        MusicaService.createMusica(musica);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMusica(@PathVariable int id) {
+        MusicaService.deleteMusica(id);
+    }
+
+    @PutMapping("/{id}")
+    public void updateMusica(@PathVariable int id, @RequestBody Musica musica) {
+        musica.setId(id);
+        MusicaService.updateMusica(musica);
+    }
+}
