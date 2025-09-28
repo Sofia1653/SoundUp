d446f140e5e4b400861ea9c45c00a98bff4c82b3
Gabrielle Mastellari <150399152+gabsvelozo@users.noreply.github.com>
2025-09-27 21:23:44 -0300

    update Artista

--- backend/src/main/java/com/soundup/soundup/model/Artista.java
+++ backend/src/main/java/com/soundup/soundup/model/Artista.java
@@ -3,14 +3,12 @@ package com.soundup.soundup.model;
 public class Artista extends Usuario{
     private int id_artista;
     private int quant_ouvintes = 0;
-    private Usuario usuario;
 
     public Artista (int id, String nome, String pais, String estado, String cidade, String email, String senha,
-                    int quantSeguidores, String telefone, int id_artista, int quant_ouvintes, Usuario usuario) {
+                    int quantSeguidores, String telefone, int id_artista, int quant_ouvintes) {
         super(id, nome, pais, estado, cidade, email, senha, quantSeguidores, telefone);
         this.id_artista = id_artista;
         this.quant_ouvintes = quant_ouvintes;
-        this.usuario = usuario;
     }
 
     public int getId_artista() {
@@ -29,11 +27,4 @@ public class Artista extends Usuario{
         this.quant_ouvintes = quant_ouvintes;
     }
 
-    public Usuario getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
-    }
 }
